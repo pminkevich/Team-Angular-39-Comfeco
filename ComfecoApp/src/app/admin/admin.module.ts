1932fb48e6e3b37fea2e338611a5b4ff50e21f6a
pminkevich <pminkevich@gmail.com>
2021-02-13 22:49:26 -0300

    add forms

--- ComfecoApp/src/app/admin/admin.module.ts
+++ ComfecoApp/src/app/admin/admin.module.ts
@@ -5,9 +5,10 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { TeamsComponent } from './teams/teams.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { LevelComponent } from './components/level/level.component';
 
 @NgModule({
-  declarations: [AdminComponent, TeamsComponent],
+  declarations: [AdminComponent, TeamsComponent, LevelComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
--- ComfecoApp/src/app/login/login.module.ts
+++ ComfecoApp/src/app/login/login.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { RegisterComponent } from './page/register/register.component';
+import { Step2Component } from './page/step2/step2.component';
+import { QuestionsComponent } from './page/questions/questions.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, Step2Component, QuestionsComponent],
   imports: [
     CommonModule,
     LoginRoutingModule
