f028cae7f27356f73af76165f96854e7147c61bf
JonathanRome <jhoropertuz@gmail.com>
2021-02-24 12:17:24 -0500

    temporizador

---
+++ ComfecoApp/src/app/home/components/countdown/countdown.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  templateUrl: './countdown.component.html',
+  styleUrls: ['./countdown.component.css']
+})
+export class CountdownComponent implements OnInit {
+  currenTime;
+  constructor() { }
+
+  ngOnInit(): void {
+      this.countdown(new Date("December 24, 2021 00:00:00"));
+  }
+  getTime(dateTo) {
+      let now = new Date(),
+          time = ( <any>new Date(dateTo) - <any>now + 1000) / 1000,
+          seconds = ('0' + Math.floor(time % 60)).slice(-2),
+          minutes = ('0' + Math.floor(time / 60 % 60)).slice(-2),
+          hours = ('0' + Math.floor(time / 3600 % 24)).slice(-2),
+          days = Math.floor(time / (3600 * 24));
+  
+      return {
+          seconds,
+          minutes,
+          hours,
+          days,
+          time
+      }
+  };
+ 
+  countdown(dateTo){
+    const timerUpdate = setInterval( () => {
+        this.currenTime = this.getTime(dateTo);
+        console.log(this.currenTime);
+        
+        if (this.currenTime.time <= 1) {
+            clearInterval(timerUpdate);
+            alert('Fin de la cuenta ');
+        }
+    }, 1000);
+  };
+
+
+
+
+
+
+
+
+
+
+}
--- ComfecoApp/src/app/home/home.module.ts
+++ ComfecoApp/src/app/home/home.module.ts
@@ -7,9 +7,10 @@ import { CardComponent } from './components/card/card.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { SponsorsComponent } from './components/sponsors/sponsors.component';
 import { SwiperModule } from 'swiper/angular';
+import { CountdownComponent } from './components/countdown/countdown.component';
 
 @NgModule({
-  declarations: [HomeComponent, CardComponent, CardsComponent, SponsorsComponent],
+  declarations: [HomeComponent, CardComponent, CardsComponent, SponsorsComponent, CountdownComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
