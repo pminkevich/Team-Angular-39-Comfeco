fded89ed3184be263c9b6eeea916f0eb3ef4e75a
jhoropertuz <jhoropertuz@gmail.com>
2021-03-04 22:58:27 -0500

    formulario actualizar perfil

--- ComfecoApp/src/app/shared/shared.module.ts
+++ ComfecoApp/src/app/shared/shared.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from '@app/shared/components-global/footer/footer.component';
 import { NavComponent } from '@app/shared/components-global/nav/nav.component';
 import { RouterModule } from '@angular/router';
-import { InsigniasComponent } from './compnents/insignias/insignias.component';
-import { CardEventsComponent } from './compnents/card-events/card-events.component';
+import { InsigniasComponent } from './components/insignias/insignias.component';
+import { CardEventsComponent } from './components/card-events/card-events.component';
 import { SwiperModule } from 'swiper/angular';
+import { FormEditUserComponent } from './components/form/form-edit-user/form-edit-user.component';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [FooterComponent,NavComponent, InsigniasComponent, CardEventsComponent],
+  declarations: [FooterComponent,NavComponent, InsigniasComponent, CardEventsComponent, FormEditUserComponent],
   imports: [
     CommonModule,
     RouterModule,
-    SwiperModule
+    SwiperModule,
+    ReactiveFormsModule
   ],
-  exports: [FooterComponent, NavComponent,InsigniasComponent,CardEventsComponent]
+  exports: [FooterComponent, NavComponent,InsigniasComponent,CardEventsComponent,FormEditUserComponent]
 })
 export class SharedModule { }
